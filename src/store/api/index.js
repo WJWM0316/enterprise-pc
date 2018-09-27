3572e33d479f0ae82695ddf55cfff6fede74c915
lizifa <lphvali@thetiger.com.cn>
2018-09-27 11:50:27 +0800

    测试19

--- src/store/api/index.js
+++ src/store/api/index.js
@@ -15,10 +15,11 @@ export const upload_api = `${window.location.origin}/tiger/attaches`
 // 请求超时时间
 axios.defaults.timeout = 10000
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.withCredentials = true
+// axios.defaults.withCredentials = true
 if(process.env.NODE_ENV === 'production') {
   axios.defaults.baseURL = 'http://web.xplus.ziwork.com/tiger/'
 }
+console.log('dddddd')
 // 请求拦截器
 axios.interceptors.request.use(
   config => {
