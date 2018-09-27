9059b5f56fc0e04afe393cc367016b0e8214b441
lizifa <lphvali@thetiger.com.cn>
2018-09-27 17:10:55 +0800

    测试30

--- src/store/api/index.js
+++ src/store/api/index.js
@@ -19,7 +19,7 @@ export const upload_api = `${window.location.origin}/tiger/attaches`
 if(process.env.NODE_ENV === 'production') {
   axios.defaults.baseURL = 'http://web.xplus.ziwork.com/tiger/'
 }
-axios.defaults.headers.common['token'] = getAccessToken()
+axios.defaults.headers.common['Authorization-User'] = `token=getAccessToken()`
 // 请求拦截器
 axios.interceptors.request.use(
   config => {
