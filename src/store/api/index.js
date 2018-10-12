7e7a1957546adbc926bbfb20cbde72d753027193
lizifa <lphvali@thetiger.com.cn>
2018-10-12 14:43:41 +0800

    测试 test分支

--- src/store/api/index.js
+++ src/store/api/index.js
@@ -4,7 +4,7 @@ import router from '@/router/index'
 let loadingInstance = null
 import { getAccessToken, removeAccessToken } from '@/store/cacheService'
 export const API_ROOT = 'http://web.xplus.ziwork.com/test'
-export const API_ROOT = 'http://web.xplus.ziwork.com/tiger'
+// export const API_ROOT = 'http://web.xplus.ziwork.com/tiger'
 // 请求的跟地址
 export const upload_api = `${API_ROOT}/attaches`
 
