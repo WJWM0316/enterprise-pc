c39b82249c7b2084826067b43601fe4ce212d893
lizifa <lphvali@thetiger.com.cn>
2018-10-19 18:00:54 +0800

    测试哦

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 
 const router = new Router({
   mode: 'history',
-  base: process.env.NODE_ENV === 'production' ? '/laohu' : '/',
+  base: process.env.NODE_ENV === 'production' ? `/${process.env.VUE_APP_COMPANY}` : '/',
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
