69c1a0eeb85eea8130f262365490983ccca70514
lizifa <lphvali@thetiger.com.cn>
2018-11-20 19:56:29 +0800

    111

--- src/pages/organization/memberPost/index.js
+++ src/pages/organization/memberPost/index.js
@@ -344,7 +344,7 @@ export default class WorkZonePost extends Vue {
         }
         const need = ['name', 'avatarId', 'groupId', 'gender', 'occupation', 'email', 'wechat', 'mobile', 'password', 'roleId', 'contentAdminGroup', 'id']
         const params = this.transformData(this.form, need)
-        // this.submit(params)
+        this.submit(params)
       }
     })
   }
