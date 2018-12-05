69e38d193ec85d247b2852a85110a24b8af27402
lizifa <lphvali@thetiger.com.cn>
2018-12-05 18:04:07 +0800

    保存前移除token

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -26,6 +26,7 @@ const state = {
 
 const mutations = {
   [LOGIN] (state, data) {
+    removeAccessToken()
     saveAccessToken(data.token, state.loginValidTime)
     saveUserInfo(data, state.loginValidTime)
     state.userInfos = data
